18dec657681d7e2503a6cb3b3ee408660af9b682
yors <aain07456@gmail.com>
2022-08-04 10:53:12 +0300

    Зделал 2 таска

--- space_simulations/Program.cs
+++ space_simulations/Program.cs
@@ -4,9 +4,15 @@ namespace space_simulations
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        static void Main(string[] args){
+            Console.WriteLine(Task1());
+        }
+
+        static byte[] Task1() {
+            return new byte[] {6 , 6, 64, 3, 8, 1, 0 };
+        }
+        static byte[] Task2(){
+            return new byte[] { 1, 8, 1, 0, 6 };
         }
     }
 }
