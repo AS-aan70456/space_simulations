eae0fd27032374c58a32a0b9bc81c24df2a6f9d2
yors <aain07456@gmail.com>
2022-08-04 11:50:02 +0300

    Добавил Task3

--- space_simulations/Program.cs
+++ space_simulations/Program.cs
@@ -5,7 +5,7 @@ namespace space_simulations
     class Program
     {
         static void Main(string[] args){
-            Console.WriteLine(Task1());
+            Console.WriteLine(Task3());
         }
 
         static byte[] Task1() {
@@ -13,6 +13,11 @@ namespace space_simulations
         }
         static byte[] Task2(){
             return new byte[] { 1, 8, 1, 0, 6 };
+        }
+
+        static byte[] Task3()
+        {
+            return new byte[] { 9, 4, 234, 43, 5, 4, 3, 43, };
         }
     }
 }
