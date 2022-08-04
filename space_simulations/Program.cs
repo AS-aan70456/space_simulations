f7cfac31aa7ee1fdb75dfd22d8288b16481b7509
yors <aain07456@gmail.com>
2022-08-04 11:48:35 +0300

    Исправил Task2

--- space_simulations/Program.cs
+++ space_simulations/Program.cs
@@ -5,14 +5,14 @@ namespace space_simulations
     class Program
     {
         static void Main(string[] args){
-            Console.WriteLine(Task1());
+            Console.WriteLine(Task2());
         }
 
         static byte[] Task1() {
             return new byte[] {6 , 6, 64, 3, 8, 1, 0 };
         }
         static byte[] Task2(){
-            return new byte[] { 1, 8, 1, 0, 6 };
+            return new byte[] { 1, 8, 1, 0, 2, 4, 2, 3 , 6, 2, 7, 1 };
         }
     }
 }
